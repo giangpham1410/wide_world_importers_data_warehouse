f19093f2f30aab9234d56ff591f39aabcde520cc
giangpham1410 <trucgiang.pn@gmail.com>
2025-07-22 21:14:49 +0700

    feat(column): customer_category_key, customer_category_name, buying_group_key, buying_group_name

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -7,6 +7,8 @@ WITH
 , dim_customer__rename_column AS (
     SELECT
       customer_id AS customer_key
+      , customer_category_id AS customer_category_key
+      , buying_group_id AS buying_group_key
       , customer_name
     FROM dim_customer__source
 )
@@ -14,11 +16,21 @@ WITH
 , dim_customer__cast_type AS (
     SELECT
       CAST(customer_key AS INTEGER) customer_key
+      , CAST(customer_category_key AS INTEGER) AS customer_category_key
+      , CAST(buying_group_key AS INTEGER) AS buying_group_key
       , CAST(customer_name AS STRING) customer_name
     FROM dim_customer__rename_column
 )
 
 SELECT
-  customer_key
-  , customer_name
-FROM dim_customer__cast_type
+    dim_customer.customer_key
+  , dim_customer.customer_category_key
+  , dim_customer_category.customer_category_name
+  , dim_customer.buying_group_key
+  , dim_buying_group.buying_group_name
+  , dim_customer.customer_name
+FROM dim_customer__cast_type dim_customer
+  LEFT JOIN {{ ref('stg_dim_customer_category') }} dim_customer_category
+    ON dim_customer.customer_category_key = dim_customer_category.customer_category_key
+  LEFT JOIN {{ ref('stg_dim_buying_group') }} dim_buying_group
+    ON dim_customer.buying_group_key = dim_buying_group.buying_group_key
