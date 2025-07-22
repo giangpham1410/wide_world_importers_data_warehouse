1f8e9b6f47ea5ce4add71fa1648b45f73cd71cb2
giangpham1410 <trucgiang.pn@gmail.com>
2025-07-22 12:01:47 +0700

    feat(column): add new product_key

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -2,5 +2,6 @@ SELECT
   order_line_id AS sales_order_line_key
   , unit_price
   , quantity
+  , stock_item_id AS product_key
   , unit_price * quantity AS gross_amount
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
