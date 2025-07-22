f1278f7d5058ac2eb6820001ccbfc700d09986cd
giangpham1410 <trucgiang.pn@gmail.com>
2025-07-22 15:16:11 +0700

    add spacebar

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -7,21 +7,21 @@ WITH
   ,dim_product__rename_column AS (
     SELECT
       stock_item_id AS product_key
-      ,stock_item_name AS product_name
-      ,brand AS brand_name
+      , stock_item_name AS product_name
+      , brand AS brand_name
     FROM dim_product__source
 )
 
   ,dim_product__cast_type AS (
     SELECT
       CAST(product_key AS INTEGER) AS product_key
-      ,CAST(product_name AS STRING) AS product_name
-      ,CAST(brand_name AS STRING) AS brand_name
+      , CAST(product_name AS STRING) AS product_name
+      , CAST(brand_name AS STRING) AS brand_name
     FROM dim_product__rename_column
 )
 
 SELECT
   product_key
-  ,product_name
-  ,brand_name
+  , product_name
+  , brand_name
 FROM dim_product__cast_type
