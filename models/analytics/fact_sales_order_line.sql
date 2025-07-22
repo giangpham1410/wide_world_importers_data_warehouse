4dd4eadda7189d7261f025e3feb937662c20ecfd
giangpham1410 <trucgiang.pn@gmail.com>
2025-07-22 18:56:57 +0700

    feat(column): sales_order_key

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -7,6 +7,7 @@ WITH
 , fact_sales_order_line__rename_column AS (
     SELECT
       order_line_id AS sales_order_line_key
+      , order_id AS sales_order_key
       , stock_item_id AS product_key
       , quantity
       , unit_price
@@ -16,6 +17,7 @@ WITH
 , fact_sales_order_line__cast_type AS (
     SELECT
       CAST(sales_order_line_key AS INTEGER) AS sales_order_line_key
+      , CAST(sales_order_key AS INTEGER) AS sales_order_key
       , CAST(product_key AS INTEGER) AS product_key
       , CAST(quantity AS INTEGER) AS quantity
       , CAST(unit_price AS NUMERIC) AS unit_price
@@ -24,6 +26,7 @@ WITH
 
 SELECT 
   sales_order_line_key
+  , sales_order_key
   , product_key
   , quantity
   , unit_price
