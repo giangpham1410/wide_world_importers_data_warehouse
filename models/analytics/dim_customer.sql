1e461c8739dc13028015faf6a96ee4c4b9d8707d
giangpham1410 <trucgiang.pn@gmail.com>
2025-07-22 15:36:46 +0700

    cte rename column

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,17 @@
+WITH
+  dim_customer__source AS (
+    SELECT *
+    FROM `vit-lam-data.wide_world_importers.sales__customers`
+)
+
+, dim_customer__rename_column AS (
+    SELECT
+      customer_id AS customer_key
+      , customer_name
+    FROM dim_customer__source
+)
+
+
 SELECT 
   *
-FROM `vit-lam-data.wide_world_importers.sales__customers`
+FROM dim_customer__rename_column
