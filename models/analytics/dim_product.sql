6f5f749514c42d1a60100b146932b035717320e1
giangpham1410 <trucgiang.pn@gmail.com>
2025-07-23 13:27:40 +0700

    fix(logic): handle null brand_name

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -4,37 +4,47 @@ WITH
     FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
 )
 
-  , dim_product__rename_column AS (
-      SELECT
-        stock_item_id AS product_key
-        , stock_item_name AS product_name
-        , supplier_id AS supplier_key
-        , brand AS brand_name
-        , is_chiller_stock AS is_chiller_stock_boolean
-      FROM dim_product__source
+, dim_product__rename_column AS (
+    SELECT
+      stock_item_id AS product_key
+      , stock_item_name AS product_name
+      , supplier_id AS supplier_key
+      , brand AS brand_name
+      , is_chiller_stock AS is_chiller_stock_boolean
+    FROM dim_product__source
 )
 
-  , dim_product__cast_type AS (
-      SELECT
-        CAST(product_key AS INTEGER) AS product_key
-        , CAST(product_name AS STRING) AS product_name
-        , CAST(supplier_key AS INTEGER) AS supplier_key
-        , CAST(brand_name AS STRING) AS brand_name
-        , CAST(is_chiller_stock_boolean AS BOOLEAN) AS is_chiller_stock_boolean
-      FROM dim_product__rename_column
+, dim_product__cast_type AS (
+    SELECT
+      CAST(product_key AS INTEGER) AS product_key
+      , CAST(product_name AS STRING) AS product_name
+      , CAST(supplier_key AS INTEGER) AS supplier_key
+      , CAST(brand_name AS STRING) AS brand_name
+      , CAST(is_chiller_stock_boolean AS BOOLEAN) AS is_chiller_stock_boolean
+    FROM dim_product__rename_column
 )
 
-  , dim_product__convert_boolean AS (
-      SELECT
-        *
-        , CASE
-            WHEN is_chiller_stock_boolean IS TRUE THEN 'Chiller Stock'
-            WHEN is_chiller_stock_boolean IS FALSE THEN 'Not Chiller Stock'
-            WHEN is_chiller_stock_boolean IS NULL THEN 'Undefined'
-            ELSE 'Invalid'
-          END AS is_chiller_stock
-      FROM dim_product__cast_type 
-  )
+, dim_product__convert_boolean AS (
+    SELECT
+      *
+      , CASE
+          WHEN is_chiller_stock_boolean IS TRUE THEN 'Chiller Stock'
+          WHEN is_chiller_stock_boolean IS FALSE THEN 'Not Chiller Stock'
+          WHEN is_chiller_stock_boolean IS NULL THEN 'Undefined'
+          ELSE 'Invalid'
+        END AS is_chiller_stock
+    FROM dim_product__cast_type 
+)
+
+, dim_product__handle_null AS (
+    SELECT
+      product_key
+      , product_name
+      , supplier_key
+      , COALESCE(brand_name, 'Undefined') AS brand_name
+      , is_chiller_stock
+    FROM dim_product__convert_boolean
+)
 
 
 SELECT
@@ -44,6 +54,6 @@ SELECT
   , dim_supplier.supplier_name
   , dim_product.brand_name
   , dim_product.is_chiller_stock
-FROM dim_product__convert_boolean dim_product
+FROM dim_product__handle_null dim_product
   LEFT JOIN {{ ref('dim_supplier') }} dim_supplier
     ON dim_product.supplier_key = dim_supplier.supplier_key
