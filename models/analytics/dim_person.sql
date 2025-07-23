f5b99b5a5bb3099a2bb0df5651e7215913bd6941
giangpham1410 <trucgiang.pn@gmail.com>
2025-07-23 14:28:42 +0700

    CTE rename column

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -1,3 +1,16 @@
-SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.application__people`
+WITH
+  dim_person__source AS (
+    SELECT 
+      *
+    FROM `vit-lam-data.wide_world_importers.application__people`
+)
+
+, dim_person__rename_column AS (
+    SELECT
+      person_id AS person_key
+      , full_name AS full_name
+    FROM dim_person__source
+)
+
+SELECT *
+FROM dim_person__rename_column
