1ebb15a5a3cb5b0233fb8b1efa956aba9e9a5885
giangpham1410 <trucgiang.pn@gmail.com>
2025-07-23 11:00:43 +0700

    feat(column): is_chiller_stock

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -4,25 +4,38 @@ WITH
     FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
 )
 
-  ,dim_product__rename_column AS (
-    SELECT
-      stock_item_id AS product_key
-      , stock_item_name AS product_name
-      , supplier_id AS supplier_key
-      , brand AS brand_name
-      , is_chiller_stock
-    FROM dim_product__source
+  , dim_product__rename_column AS (
+      SELECT
+        stock_item_id AS product_key
+        , stock_item_name AS product_name
+        , supplier_id AS supplier_key
+        , brand AS brand_name
+        , is_chiller_stock
+      FROM dim_product__source
 )
 
-  ,dim_product__cast_type AS (
-    SELECT
-      CAST(product_key AS INTEGER) AS product_key
-      , CAST(product_name AS STRING) AS product_name
-      , CAST(supplier_key AS INTEGER) AS supplier_key
-      , CAST(brand_name AS STRING) AS brand_name
-      , CAST(is_chiller_stock AS BOOLEAN) AS is_chiller_stock
-    FROM dim_product__rename_column
+  , dim_product__cast_type AS (
+      SELECT
+        CAST(product_key AS INTEGER) AS product_key
+        , CAST(product_name AS STRING) AS product_name
+        , CAST(supplier_key AS INTEGER) AS supplier_key
+        , CAST(brand_name AS STRING) AS brand_name
+        , CAST(is_chiller_stock AS BOOLEAN) AS is_chiller_stock_boolean
+      FROM dim_product__rename_column
 )
+
+  , dim_product__convert_boolean AS (
+      SELECT
+        *
+        , CASE
+            WHEN is_chiller_stock_boolean IS TRUE THEN 'Chiller Stock'
+            WHEN is_chiller_stock_boolean IS FALSE THEN 'Not Chiller Stock'
+            ELSE 'Undefined'
+          END AS is_chiller_stock
+      FROM dim_product__cast_type 
+  )
+
+
 
 SELECT
   dim_product.product_key
@@ -31,6 +44,6 @@ SELECT
   , dim_supplier.supplier_name
   , dim_product.brand_name
   , dim_product.is_chiller_stock
-FROM dim_product__cast_type dim_product
+FROM dim_product__convert_boolean dim_product
   LEFT JOIN {{ ref('dim_supplier') }} dim_supplier
     ON dim_product.supplier_key = dim_supplier.supplier_key
