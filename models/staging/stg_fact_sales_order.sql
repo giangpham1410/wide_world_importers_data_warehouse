188a9620cb5781d23da83485680bd52c00e3f747
giangpham1410 <trucgiang.pn@gmail.com>
2025-07-23 14:47:25 +0700

    feat(column): picked_by_person_key

--- models/staging/stg_fact_sales_order.sql
+++ models/staging/stg_fact_sales_order.sql
@@ -8,6 +8,7 @@ WITH
     SELECT
       order_id AS sales_order_key
       , customer_id AS customer_key
+      , picked_by_person_id AS picked_by_person_key
     FROM fact_sales_order__source
 )
 
@@ -15,10 +16,12 @@ WITH
     SELECT
       CAST(sales_order_key AS INTEGER) AS sales_order_key
       , CAST(customer_key AS INTEGER) AS customer_key
+      , CAST(picked_by_person_key AS INTEGER) AS picked_by_person_key
     FROM fact_sales_order__rename_column
 )
 
 SELECT
   sales_order_key
   , customer_key
+  , picked_by_person_key
 FROM fact_sales_order__cast_type
