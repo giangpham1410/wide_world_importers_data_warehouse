da1a3e15f9e0e060bec00775310589c014671872
giangpham1410 <trucgiang.pn@gmail.com>
2025-07-25 19:28:53 +0700

    add(CTE): cast type

--- models/staging/stg_dim_state_province.sql
+++ models/staging/stg_dim_state_province.sql
@@ -14,6 +14,16 @@ WITH
     FROM dim_state_province__source
 )
 
+, dim_state_province__cast_type AS (
+    SELECT
+      CAST(state_province_key AS INTEGER) AS state_province_key
+      , CAST(state_province_name AS STRING) AS state_province_name
+      , CAST(state_province_code AS STRING) AS state_province_code
+      , CAST(sales_territory AS STRING) AS sales_territory
+      , CAST(country_key AS INTEGER) AS country_key
+    FROM dim_state_province__rename_column
+)
+
 
 SELECT *
-FROM dim_state_province__rename_column
+FROM dim_state_province__cast_type
