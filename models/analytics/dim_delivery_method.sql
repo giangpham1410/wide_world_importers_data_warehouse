a36703f031fe9811f01b9f9b54ac29d44e3b43ef
giangpham1410 <trucgiang.pn@gmail.com>
2025-07-25 20:20:37 +0700

    add(CTE): add undefined record

--- models/analytics/dim_delivery_method.sql
+++ models/analytics/dim_delivery_method.sql
@@ -18,6 +18,25 @@ WITH
     FROM dim_delivery_method__rename_column
 )
 
+, dim_delivery_method__add_undefined_record AS (
+    SELECT
+      delivery_method_key
+      , delivery_method_name
+    FROM dim_delivery_method__cast_type
 
-SELECT *
-FROM dim_delivery_method__cast_type
+    UNION ALL
+    SELECT
+      0 AS delivery_method_key
+      , 'Undefined' AS delivery_method_name
+
+    UNION ALL
+    SELECT
+      -1 AS delivery_method_key
+      , 'Invalid' AS delivery_method_name
+)
+
+
+SELECT
+  delivery_method_key
+  , delivery_method_name
+FROM dim_delivery_method__add_undefined_record
