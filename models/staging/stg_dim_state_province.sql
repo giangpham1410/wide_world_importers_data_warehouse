53e6e3bd21b881e261618c32f04975aa490bbf42
giangpham1410 <trucgiang.pn@gmail.com>
2025-07-25 19:40:14 +0700

    add(CTE): add undefined record

--- models/staging/stg_dim_state_province.sql
+++ models/staging/stg_dim_state_province.sql
@@ -24,6 +24,42 @@ WITH
     FROM dim_state_province__rename_column
 )
 
+, dim_state_province__add_undefined_record AS (
+    SELECT
+      state_province_key
+      , state_province_name
+      , state_province_code
+      , sales_territory
+      , country_key
+    FROM dim_state_province__cast_type
 
-SELECT *
-FROM dim_state_province__cast_type
+    UNION ALL
+    SELECT
+      0 AS state_province_key
+      , 'Undefined' AS state_province_name
+      , 'Undefined' AS state_province_code
+      , 'Undefined' AS sales_territory
+      , 0 AS country_key
+
+    UNION ALL
+    SELECT
+      -1 AS state_province_key
+      , 'Invalid' AS state_province_name
+      , 'Invalid' AS state_province_code
+      , 'Invalid' AS sales_territory
+      , -1 AS country_key
+)
+
+
+SELECT
+    dim_state_province.state_province_key
+  , dim_state_province.state_province_name
+  , dim_state_province.state_province_code
+  , dim_state_province.sales_territory
+  , dim_state_province.country_key
+  , COALESCE(dim_country.country_name, 'Invalid') AS country_name
+  , COALESCE(dim_country.country_code, 'Invalid') AS country_code
+  , COALESCE(dim_country.region, 'Invalid') AS region
+FROM dim_state_province__add_undefined_record AS dim_state_province
+  LEFT JOIN {{ ref('stg_dim_country') }} AS dim_country
+    ON dim_state_province.country_key = dim_country.country_key
