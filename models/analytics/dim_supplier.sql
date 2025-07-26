b89b3013b132c44b5ff52180eba05f6fd5733640
giangpham1410 <trucgiang.pn@gmail.com>
2025-07-26 11:27:57 +0700

    feat(column): retrieve multiple columns

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -9,6 +9,12 @@ WITH
     SELECT
       supplier_id AS supplier_key
       , supplier_name
+      , supplier_category_id AS supplier_category_key
+      , delivery_method_id AS delivery_method_key
+      , delivery_city_id AS delivery_city_key
+      , postal_city_id AS postal_city_key
+      , primary_contact_person_id AS primary_contact_person_key
+      , payment_days
     FROM dim_supplier__source
 )
 
@@ -16,6 +22,12 @@ WITH
     SELECT
       CAST(supplier_key AS INTEGER) AS supplier_key
       , CAST(supplier_name AS STRING) AS supplier_name
+      CAST(supplier_category_key AS INTEGER) AS supplier_category_key
+      CAST(delivery_method_key AS INTEGER) AS delivery_method_key
+      CAST(delivery_city_key AS INTEGER) AS delivery_city_key
+      CAST(postal_city_key AS INTEGER) AS postal_city_key
+      CAST(primary_contact_person_key AS INTEGER) AS primary_contact_person_key
+      CAST(payment_days AS INTEGER) AS payment_days
     FROM dim_supplier__rename_column
 )
 
@@ -23,21 +35,44 @@ WITH
     SELECT
       supplier_key
       , supplier_name
+      , supplier_category_key
+      , delivery_method_key
+      , delivery_city_key
+      , postal_city_key
+      , primary_contact_person_key
+      , payment_days
     FROM dim_supplier__cast_type
 
     UNION ALL
     SELECT
       0 AS supplier_key
       , 'Undefined' AS supplier_name
+      , 0 AS supplier_category_key
+      , 0 AS delivery_method_key
+      , 0 AS delivery_city_key
+      , 0 AS postal_city_key
+      , 0 AS primary_contact_person_key
+      , NULL AS payment_days
 
     UNION ALL
     SELECT
       -1 AS supplier_key
       , 'Invalid' AS supplier_name
+      , -1 AS supplier_category_key
+      , -1 AS delivery_method_key
+      , -1 AS delivery_city_key
+      , -1 AS postal_city_key
+      , -1 AS primary_contact_person_key
+      , NULL AS payment_days
 
 )
 
 SELECT
   supplier_key
   , supplier_name
+  , supplier_category_key
+  , delivery_method_key
+  , delivery_city_key
+  , postal_city_key
+  , primary_contact_person_key
 FROM dim_supplier__add_undefined_record
