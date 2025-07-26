3471ca7a50a72c1a512b0bcd98687621b8a37671
giangpham1410 <trucgiang.pn@gmail.com>
2025-07-26 11:34:14 +0700

    add(CTE): handle null

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -22,13 +22,26 @@ WITH
     SELECT
       CAST(supplier_key AS INTEGER) AS supplier_key
       , CAST(supplier_name AS STRING) AS supplier_name
-      CAST(supplier_category_key AS INTEGER) AS supplier_category_key
-      CAST(delivery_method_key AS INTEGER) AS delivery_method_key
-      CAST(delivery_city_key AS INTEGER) AS delivery_city_key
-      CAST(postal_city_key AS INTEGER) AS postal_city_key
-      CAST(primary_contact_person_key AS INTEGER) AS primary_contact_person_key
-      CAST(payment_days AS INTEGER) AS payment_days
+      , CAST(supplier_category_key AS INTEGER) AS supplier_category_key
+      , CAST(delivery_method_key AS INTEGER) AS delivery_method_key
+      , CAST(delivery_city_key AS INTEGER) AS delivery_city_key
+      , CAST(postal_city_key AS INTEGER) AS postal_city_key
+      , CAST(primary_contact_person_key AS INTEGER) AS primary_contact_person_key
+      , CAST(payment_days AS INTEGER) AS payment_days
     FROM dim_supplier__rename_column
+)
+
+, dim_supplier__handle_null AS (
+    SELECT
+      supplier_key
+      , supplier_name
+      , COALESCE(supplier_category_key, 0) AS supplier_category_key
+      , COALESCE(delivery_method_key, 0) AS delivery_method_key
+      , COALESCE(delivery_city_key, 0) AS delivery_city_key
+      , COALESCE(postal_city_key, 0) AS postal_city_key
+      , COALESCE(primary_contact_person_key, 0) AS primary_contact_person_key
+      , payment_days
+    FROM dim_supplier__cast_type
 )
 
 , dim_supplier__add_undefined_record AS (
@@ -41,7 +54,7 @@ WITH
       , postal_city_key
       , primary_contact_person_key
       , payment_days
-    FROM dim_supplier__cast_type
+    FROM dim_supplier__handle_null
 
     UNION ALL
     SELECT
