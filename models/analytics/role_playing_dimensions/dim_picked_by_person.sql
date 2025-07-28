dcf6953b39f138fa491f869895cfa7569b7a413e
giangpham1410 <trucgiang.pn@gmail.com>
2025-07-28 20:26:09 +0700

    feat(table): dim_picked_by_person

---
+++ models/analytics/role_playing_dimensions/dim_picked_by_person.sql
@@ -0,0 +1,8 @@
+SELECT
+  person_key AS picked_by_person_key
+  , full_name AS picked_by_full_name
+  , preferred_name AS picked_by_preferred_name
+FROM {{ ref('dim_person') }}
+WHERE
+  is_employee = 'Employee'
+  OR person_key IN (0, -1)
