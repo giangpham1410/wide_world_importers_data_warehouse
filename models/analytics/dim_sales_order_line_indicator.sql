40b42e421b2299fe6aa960ac48af6f8c1cf3a8bb
giangpham1410 <trucgiang.pn@gmail.com>
2025-07-30 14:49:18 +0700

    fix(logic): sales_order_line_indicator_key

--- models/analytics/dim_sales_order_line_indicator.sql
+++ models/analytics/dim_sales_order_line_indicator.sql
@@ -1,14 +1,24 @@
 WITH
   dim_is_undersupply_backordered AS (
     SELECT
-      1 AS is_undersupply_backordered_key
+      TRUE AS is_undersupply_backordered_key
       , 'Undersupply Backordered' AS is_undersupply_backordered
+
     UNION ALL
     SELECT
-      2 AS is_undersupply_backordered_key
+      FALSE AS is_undersupply_backordered_key
       , 'Not Undersupply Backordered' AS is_undersupply_backordered
 )
-SELECT *
-FROM dim_is_undersupply_backordered
+SELECT
+  CONCAT(
+    dim_is_undersupply_backordered.is_undersupply_backordered
+    , ','
+    , dim_package_type.package_type_key)
+    AS sales_order_line_indicator_key
+  , dim_is_undersupply_backordered.is_undersupply_backordered_key
+  , dim_is_undersupply_backordered.is_undersupply_backordered
+  , dim_package_type.package_type_key
+  , dim_package_type.package_type_name
+FROM dim_is_undersupply_backordered AS dim_is_undersupply_backordered
   CROSS JOIN {{ ref('dim_package_type') }} AS dim_package_type
 ORDER BY 1,3
