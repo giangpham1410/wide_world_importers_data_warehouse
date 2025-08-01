5fe0fc4fb0e39120f3b103c39699c8b60d6d3666
giangpham1410 <trucgiang.pn@gmail.com>
2025-08-01 10:38:37 +0700

    update(PK): salesperson_person_key

--- models/analytics/role_playing_dimensions/dim_salesperson_person.sql
+++ models/analytics/role_playing_dimensions/dim_salesperson_person.sql
@@ -1,5 +1,5 @@
 SELECT
-  person_key AS salesperson_key
+  person_key AS salesperson_person_key
   , full_name AS salesperson_full_name
   , preferred_name AS salesperson_preferred_name
 FROM {{ ref('dim_person') }}
