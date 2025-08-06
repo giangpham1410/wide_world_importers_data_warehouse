9171e49d4fcfd713c2b1f225be0f51c2db64075f
giangpham1410 <trucgiang.pn@gmail.com>
2025-08-06 20:19:43 +0700

    add(CTE): handle null, join in main query

--- models/analytics/fact_invoice_line.sql
+++ models/analytics/fact_invoice_line.sql
@@ -36,17 +36,63 @@ WITH
     FROM fact_invoice_line__rename_column
 )
 
+, fact_invoice_line__handle_null AS (
+    SELECT
+      invoice_line_key
+      , description
+      , COALESCE(invoice_key, 0) AS invoice_key
+      , COALESCE(product_key, 0) AS product_key
+      , COALESCE(package_type_key, 0) AS package_type_key
+      , quantity
+      , unit_price
+      , tax_rate
+      , tax_amount
+      , line_profit
+      , extended_price
+    FROM fact_invoice_line__cast_type
+)
+
 
 SELECT
-  invoice_line_key
-  , description
-  , invoice_key
-  , product_key
-  , package_type_key
-  , quantity
-  , unit_price
-  , tax_rate
-  , tax_amount
-  , line_profit
-  , extended_price
-FROM fact_invoice_line__cast_type
+    fact_invoice_line.invoice_line_key
+  , fact_invoice_line.description
+
+  -- Invoice Header Information
+  , fact_invoice_header.is_credit_note_boolean
+  , fact_invoice_header.credit_note_reason
+  , fact_invoice_header.customer_purchase_order_number
+  , fact_invoice_header.invoice_date
+  , fact_invoice_header.confirmed_delivery_at
+  , fact_invoice_header.confirmed_received_by
+  , fact_invoice_header.total_dry_items
+  , fact_invoice_header.total_chiller_items
+  , fact_invoice_header.received_by
+  , fact_invoice_header.con_note
+  , fact_invoice_header.status
+  , fact_invoice_header.delivered_at
+
+  -- Invoice Line - FK 
+  , fact_invoice_line.invoice_key
+  , fact_invoice_line.product_key
+  , fact_invoice_line.package_type_key
+
+  -- Invoice Header - FK
+  , COALESCE(fact_invoice_header.sales_order_key, -1) AS sales_order_key
+  , COALESCE(fact_invoice_header.customer_key, -1) AS customer_key
+  , COALESCE(fact_invoice_header.bill_to_customer_key, -1) AS bill_to_customer_key
+  , COALESCE(fact_invoice_header.delivery_method_key, -1) AS delivery_method_key
+  , COALESCE(fact_invoice_header.contact_person_Key, -1) AS contact_person_Key
+  , COALESCE(fact_invoice_header.accounts_person_key, -1) AS accounts_person_key
+  , COALESCE(fact_invoice_header.salesperson_person_key, -1) AS salesperson_person_key
+  , COALESCE(fact_invoice_header.packed_by_person_key, -1) AS packed_by_person_key
+
+  -- Measure
+  , fact_invoice_line.quantity
+  , fact_invoice_line.unit_price
+  , fact_invoice_line.tax_rate
+  , fact_invoice_line.tax_amount
+  , fact_invoice_line.line_profit
+  , fact_invoice_line.extended_price
+FROM fact_invoice_line__handle_null AS fact_invoice_line
+  LEFT JOIN {{ ref('stg_fact_invoice') }} AS fact_invoice_header
+    ON fact_invoice_line.invoice_key = fact_invoice_header.invoice_key
