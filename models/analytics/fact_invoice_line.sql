c26af4b00f4b1f4e2f7879227dfc7a4151100333
giangpham1410 <trucgiang.pn@gmail.com>
2025-08-06 20:06:49 +0700

    add(CTE): cast type

--- models/analytics/fact_invoice_line.sql
+++ models/analytics/fact_invoice_line.sql
@@ -20,6 +20,33 @@ WITH
     FROM fact_invoice_line__source
 )
 
+, fact_invoice_line__cast_type AS (
+    SELECT
+      CAST(invoice_line_key AS INTEGER) AS invoice_line_key
+      , CAST(description AS STRING) AS description
+      , CAST(invoice_key AS INTEGER) AS invoice_key
+      , CAST(product_key AS INTEGER) AS product_key
+      , CAST(package_type_key AS INTEGER) AS package_type_key
+      , CAST(quantity AS INTEGER) AS quantity
+      , CAST(unit_price AS NUMERIC) AS unit_price
+      , CAST(tax_rate AS NUMERIC) AS tax_rate
+      , CAST(tax_amount AS NUMERIC) AS tax_amount
+      , CAST(line_profit AS NUMERIC) AS line_profit
+      , CAST(extended_price AS NUMERIC) AS extended_price
+    FROM fact_invoice_line__rename_column
+)
 
-SELECT *
-FROM fact_invoice_line__rename_column
+
+SELECT
+  invoice_line_key
+  , description
+  , invoice_key
+  , product_key
+  , package_type_key
+  , quantity
+  , unit_price
+  , tax_rate
+  , tax_amount
+  , line_profit
+  , extended_price
+FROM fact_invoice_line__cast_type
